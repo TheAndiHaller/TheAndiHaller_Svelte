7a2c6f05338355d34d9424176d8899e49cfc6b2d
Andi <andi_haller@hotmail.com>
2024-09-21 19:55:54 -0400

    add SSL option to postgres

--- TheAndiHaller/src/lib/server/database.ts
+++ TheAndiHaller/src/lib/server/database.ts
@@ -11,8 +11,10 @@ if (!databaseUrl) {
   throw new Error('DATABASE_URL is not defined in the environment variables.');
 }
 
-// Initialize Sequelize with your database configuration
 export const sequelize = new Sequelize(databaseUrl, {
-  dialect: 'postgres', // or 'mysql', 'sqlite', etc.
-  logging: false, // Disable logging or configure it
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,  // For SSL if required in production
+  },
+  logging: false,
 });
